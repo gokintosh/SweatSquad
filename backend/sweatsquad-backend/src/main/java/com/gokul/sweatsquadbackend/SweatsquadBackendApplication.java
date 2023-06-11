f2cbacb76ed16d946f81b4921363503a83d21ea9
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2023-06-11 21:17:02 +0200

    init project

---
+++ backend/sweatsquad-backend/src/main/java/com/gokul/sweatsquadbackend/SweatsquadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.sweatsquadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweatsquadBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweatsquadBackendApplication.class, args);
+    }
+
+}
---
+++ backend/sweatsquad-backend/src/test/java/com/gokul/sweatsquadbackend/SweatsquadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.sweatsquadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweatsquadBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
